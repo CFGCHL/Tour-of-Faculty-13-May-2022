b5ded290c0b4f930342d98619a22260109e16bf5
CFGCHL <105398659+CFGCHL@users.noreply.github.com>
2022-05-13 15:00:44 +0800

    Tour Faculty of Law
    Tour Faculty of Law

---
+++ data.js
@@ -0,0 +1,164 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stone-plate-of-nus-law",
+      "name": "Stone Plate of NUS Law",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0601158628690897,
+          "pitch": 0.22428263707881335,
+          "rotation": 6.283185307179586,
+          "target": "1-c-j-koh-law-library"
+        },
+        {
+          "yaw": 0.0008869259000015006,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-c-j-koh-law-library"
+        },
+        {
+          "yaw": 0.0008869259000015006,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-c-j-koh-law-library"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2926546651773858,
+          "pitch": 0.007212120505002417,
+          "title": "Welcome&nbsp;",
+          "text": "Faculty of Law"
+        },
+        {
+          "yaw": -2.8277039147148173,
+          "pitch": 0.18697872927646308,
+          "title": "Welcome",
+          "text": "Faculty of Law"
+        }
+      ]
+    },
+    {
+      "id": "1-c-j-koh-law-library",
+      "name": "C J Koh Law Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0002956078010658558,
+          "pitch": 0.24712071649092948,
+          "rotation": 0,
+          "target": "2-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "CJ Koh Law Library",
+          "text": "Faculty of Law"
+        }
+      ]
+    },
+    {
+      "id": "2-the-summit-canteen",
+      "name": "The Summit Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.130663082124874,
+          "pitch": -0.010826785614735712,
+          "rotation": 1.5707963267948966,
+          "target": "0-stone-plate-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Law Canteen",
+          "text": "Faculty of Law"
+        }
+      ]
+    }
+  ],
+  "name": "Tour Faculty of Law",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
